77b8232859e4c01ac57c1a9862baefda6ce68a4c
Slavjan <khaidayev@gmail.com>
2016-05-29 20:59:26 +0600

    complite

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main()
+{
+    char symbol,
+         str[24];
+
+    int  number1 = 0.0,
+         number2 = 0.0;
+
+    printf("Entering a symbol: ");
+    scanf("%c", &symbol);
+    printf("Your symbol: %c\n", symbol);
+
+    printf("\n------------------\n");
+
+    printf("Entering a string: ");
+    scanf("%s", &str);
+    printf("Your string: %s", str);
+
+    printf("\n------------------\n");
+
+    printf("Entering a positiv number: ");
+    scanf("%d", &number1);
+    printf("Entering a negotiv number: ");
+    scanf("%d", &number2);
+
+    printf("Your numbers is decimal: %d, %d\n"
+           "is hex: #%x, #%x", number1, number2, number1, number2);
+
+    return 0;
+}
